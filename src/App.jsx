df30755f6818aeb94c4a7a2666e8243407f1d914
pengu2004 <tejusria02@gmail.com>
2026-02-12 10:37:08 +0530

    added slideshow but wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
 import "./App.css";
 import Editor from "./components/Editor/Editor";
+import Slideshow from "./components/SlideShow/SlideShow";
 export function App() {
   const [view, setView] = useState("editor");
 
   return (
-    <div className="h-screen flex items-center justify-center bg-black">
-      {view === "editor" ? <Editor /> : <Slideshow />}
+    <div className="h-screen flex items-center justify-center ">
+      {view === "editor" ? (
+        <Editor
+          Present={() => {
+            setView("slideshow");
+            console.log("heyy");
+          }}
+        />
+      ) : (
+        <Slideshow />
+      )}
     </div>
   );
 }
---
+++ src/components/EmptySlide.jsx
@@ -0,0 +1,3 @@
+export default function EmptySlide() {
+  return <div className="h-34  m-2 w-70 bg-gray-50 rounded-2xl  border "></div>;
+}
