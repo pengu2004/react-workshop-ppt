e17903fc9b52e0410fb28c622a2acb22f84aaae0
pengu2004 <tejusria02@gmail.com>
2026-02-12 10:46:41 +0530

    fixed numbering issue

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -1,25 +1,26 @@
 import TopBar from "./TopBar";
 import SecondaryMenu from "./SecondaryMenu";
 import EmptySlide from "../EmptySlide";
-export default function Editor({ Present}) {
+export default function Editor({ Present }) {
   return (
     <div className="h-screen w-full flex flex-col ">
-      <TopBar Present= {Present} />
+      <TopBar Present={Present} />
       <div className="px-4 py-4">
-        <SecondaryMenu  />
+        <SecondaryMenu />
       </div>
       <div className="grid grid-cols-4 gap-4 px-4 py-4">
         <div className=" col-span-1">
           <div className=" w-full   rounded-md flex flex-col items-start justify-center">
             {Array.from({ length: 9 }).map((_, index) => (
               <div key={index}>
-                <h1>{ index+1}</h1>
-                <EmptySlide />
+                <EmptySlide page={index + 1} />
               </div>
             ))}
           </div>
         </div>
-        <h2 className="border col-span-3">heee</h2>
+        <h2 className="border col-span-3 rounded-md h-150 border-t ">
+          haeee
+        </h2>
       </div>
     </div>
   );
--- src/components/EmptySlide.jsx
+++ src/components/EmptySlide.jsx
@@ -1,3 +1,8 @@
-export default function EmptySlide() {
-  return <div className="h-34  m-2 w-70 bg-gray-50 rounded-2xl  border "></div>;
+export default function EmptySlide({ page }) {
+  return (
+    <div className="flex gap-2 py-2 px-2 border-r-2">
+      <div>{page}</div>
+      <div className="h-34   w-70 bg-gray-50 rounded-2xl  border "></div>
+    </div>
+  );
 }
